e5a48f070be07232e8885a2b8123f275a9bb9dc1
Joseph Rios Henao <joseph232005@gmail.com>
2020-10-26 10:34:20 -0500

    Modifique Menu Inicial

--- src/JAVA/CalculatorTerminal.java
+++ src/JAVA/CalculatorTerminal.java
@@ -1,7 +1,16 @@
-public class CalculatorTerminal {
-    public static void main (String[] args){
-        public void MenuInicial() {
+package JAVA;
 
-        }
+public class CalculatorTerminal {
+    public static void main(String[] args){
+        PreguntarLVL();
+        MenuInicial();
+    }
+
+    public static void MenuInicial() {
+        System.out.println("hola");
+    }
+
+    public static void PreguntarLVL() {
+
     }
 }
