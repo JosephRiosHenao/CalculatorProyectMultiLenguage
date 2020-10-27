0e7d44cc3dc33bb403664ea4f0f3b6bfeab53aef
Joseph Rios Henao <joseph232005@gmail.com>
2020-10-27 13:07:12 -0500

    Complete la pregunta del modo

--- src/JAVA/Calculator.java
+++ src/JAVA/Calculator.java
@@ -1,16 +1,31 @@
 package JAVA;
 
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args){
-        PreguntarModo();
+        int ModoElegido = PreguntarModo(0);
         PreguntarLVL();
         MenuInicial();
     }
-    public static void PreguntarModo() {
-        
+    
+    public static int PreguntarModo(int Modo) {
+        boolean Ciclo = true;
+        while (Ciclo) {
+            System.out.println("Digite el Modo de la calculadora \n 1. Console \n 2. JOptionPane \n 3. GUI");
+            Scanner Entrada = new Scanner(System.in);
+            Modo = Entrada.nextInt();
+            if (Modo == 1 || Modo == 2 || Modo == 3){
+                System.out.println("--Modo registrado--");
+                break;
+            }else{
+                System.out.println("Digite un indice valido");
+            }
+        }
+        return Modo;
     }
+
     public static void PreguntarLVL() {
-        System.out.println("Digite el nivel de calculadora \n 1.F");
     }    
     public static void MenuInicial() {
         System.out.println("hola");
