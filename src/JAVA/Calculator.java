b87dc39a4e6d774c96c3427fdd056c09517e4859
Joseph Rios Henao <joseph232005@gmail.com>
2020-10-27 12:23:54 -0500

    MClean:Modo añadido

---
+++ src/JAVA/Calculator.java
@@ -0,0 +1,20 @@
+package JAVA;
+
+public class Calculator {
+    public static void main(String[] args){
+        PreguntarModo();
+        PreguntarLVL();
+        MenuInicial();
+    }
+    public static void PreguntarModo() {
+        
+    }
+    public static void PreguntarLVL() {
+        System.out.println("Digite el nivel de calculadora \n 1.F");
+    }    
+    public static void MenuInicial() {
+        System.out.println("hola");
+    }
+
+
+}
