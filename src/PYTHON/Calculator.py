1747409d4c4d7e96f978e39c045ccfc1f8bbf7b3
Joseph Rios Henao <joseph232005@gmail.com>
2020-10-29 20:00:34 -0500

    Practicando String en py

--- src/PYTHON/Calculator.py
+++ src/PYTHON/Calculator.py
@@ -1,2 +1,28 @@
 #gg
-print("hola mundo")
+myStr = "Hello world."
+
+print(type(myStr))
+#print(dir(myStr))
+
+print(myStr.upper())
+print(myStr.lower())
+
+print(myStr.swapcase())
+print(myStr.capitalize())
+
+print(myStr.replace("Hello","bye"))
+print(myStr.count("l"))
+
+print(myStr.startswith("Hello"))
+print(myStr.endswith("orld"))
+
+print(myStr.split("o"))
+print(myStr.find("."))
+
+print(len(myStr))
+print(myStr.index("."))
+
+print(myStr.isnumeric())
+print(myStr.isalpha())
+
+print(myStr[4])
