598d4d9652056e8eb151bb9d862c337f93694541
Joseph Rios Henao <joseph232005@gmail.com>
2020-10-29 20:54:55 -0500

    Practicando listas py

--- src/PYTHON/Calculator.py
+++ src/PYTHON/Calculator.py
@@ -1,3 +1,11 @@
-num1, num2 = input("Digite el primer valor: "), input("Digite el segundo valor")
-res = int(num1) + int(num2)
-print(res)
+Lista = [1,"hello",1.45,True]
+colors = ["yellow","blue","purple","brown"]
+
+number_list = list((0,1,2,3,4))
+print(number_list)
+
+r = list(range(0,10))
+print(r)
+
+print("yellow" in colors)
+
