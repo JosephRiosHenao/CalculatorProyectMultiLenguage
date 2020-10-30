709ba02eae760f5636652be18839e6db436d38a6
Joseph Rios Henao <joseph232005@gmail.com>
2020-10-29 20:22:57 -0500

    Suma practica en py

--- src/PYTHON/Calculator.py
+++ src/PYTHON/Calculator.py
@@ -1,28 +1,3 @@
-#gg
-myStr = "Hello world."
-
-print(type(myStr))
-#print(dir(myStr))
-
-print(myStr.upper())
-print(myStr.lower())
-
-print(myStr.swapcase())
-print(myStr.capitalize())
-
-print(myStr.replace("Hello","bye"))
-print(myStr.count("l"))
-
-print(myStr.startswith("Hello"))
-print(myStr.endswith("orld"))
-
-print(myStr.split("o"))
-print(myStr.find("."))
-
-print(len(myStr))
-print(myStr.index("."))
-
-print(myStr.isnumeric())
-print(myStr.isalpha())
-
-print(myStr[4])
+num1, num2 = input("Digite el primer valor: "), input("Digite el segundo valor")
+res = int(num1) + int(num2)
+print(res)
