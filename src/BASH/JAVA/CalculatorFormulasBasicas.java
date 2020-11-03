859e8907939d703b34ba84e77cec31cd8667f7f5
Joseph Rios Henao <joseph232005@gmail.com>
2020-11-02 20:57:05 -0500

    Añadi una clase para formulas basicas y añadi un ciclo

---
+++ src/BASH/JAVA/CalculatorFormulasBasicas.java
@@ -0,0 +1,20 @@
+package BASH.JAVA;
+
+import java.util.Scanner;
+
+public class CalculatorFormulasBasicas {
+    Scanner Entrada = new Scanner(System.in);
+    public int[] num;
+    public int NumerosMultiples(){
+        int CantidadAEvaluar = 0;
+        System.out.println("Digite cantidad de numeros a digitar");
+        CantidadAEvaluar = Entrada.nextInt();
+        
+
+        return Cantidad;
+    }
+
+    public float Sumar(int NumerosDeNumeros){
+
+    }
+}
--- src/BASH/JAVA/CalculatorMain.java
+++ src/BASH/JAVA/CalculatorMain.java
@@ -4,15 +4,33 @@ public class CalculatorMain {
 
     public static int OperacionElegida;
     public static boolean NivelCalcladora;
+    public static boolean CicloDeCalculadoraBasica;
 
     public static void main(String[] args) {
         NivelCalcladora = BASH.JAVA.CalculatorConsole.NivelCalculadora();
-
         if (NivelCalcladora){ //Avanzado
-
-        }else{ //Basico
-            OperacionElegida = BASH.JAVA.CalculatorConsole.MenuBasico();
-        }
         
+        }else{ //Basico
+            CicloDeCalculadoraBasica = true;
+            while (CicloDeCalculadoraBasica){
+                OperacionElegida = BASH.JAVA.CalculatorConsole.MenuBasico();
+                if (OperacionElegida == 1){ //Suma
+    
+                }else if (OperacionElegida == 2){ //Resta
+    
+                }else if (OperacionElegida == 3){ //Multiplicacion
+    
+                }else if (OperacionElegida == 4){ //Division
+
+                }else if (OperacionElegida == 5){ //Potenciacion
+
+                }else if (OperacionElegida == 6){ //Radicacion
+
+                }else{//Volver al inicio
+                    CicloDeCalculadoraBasica = false;
+                }
+            }
+            
+        }   
     }
 }
