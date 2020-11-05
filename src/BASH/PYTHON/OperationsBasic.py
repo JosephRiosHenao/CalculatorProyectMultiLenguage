55a7bf728f85595443f71049184fc958eb06a641
Joseph Rios Henao <joseph232005@gmail.com>
2020-11-05 09:59:44 -0500

    Mejorando metodo suma py

--- src/BASH/PYTHON/OperationsBasic.py
+++ src/BASH/PYTHON/OperationsBasic.py
@@ -3,13 +3,21 @@ def Sumar():
     while (CicloFunciones):
         CantidadSinEvaluar = int(input("Digite la cantidad de numeros a sumar"))
         if (CantidadSinEvaluar>=10):
-            cantidad = CantidadSinEvaluar
-            Resultado = SumarVarios(cantidad)
-        elif (CantidadSinEvaluar<=10):
+            ConfirmacionDeNumero = True            
+            while (ConfirmacionDeNumero):
+                Confirmacion = int(input(f"Estas seguro de sumar {CantidadSinEvaluar} numeros"))
+                if (Confirmacion==1 or Confirmacion==2):
+                    ConfirmacionDeNumero = False
+                    cantidad = CantidadSinEvaluar
+                    Resultado = SumarVarios(cantidad)
+                else:
+                    print("--Digite un dato valido--")
+        elif (CantidadSinEvaluar<=10 and CantidadSinEvaluar>= 0):
+            Resultado = SumarPocos(cantidad)
     return Resultado
 
 
-def SumarVarios():
+def SumarVarios(cantidad):
     NumeroEnCola = [None]*(cantidad+1)
     NumeroEnCola[0] = 0
     for Indice in len(NumeroEnCola):
@@ -17,3 +25,5 @@ def SumarVarios():
     Resultado = 0
 
     return Resultado
+
+def SumaPocos()
